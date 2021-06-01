08c88ed6fec3b0ff26dc4d64253490f398dba50f
itsokayitsme <jcao0928@gmail.com>
2021-06-01 09:21:36 -0700

    testing

---
+++ dashndine/src/Components/User.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Cart extends React.Component {
+  render() {
+    return <h2>Testing</h2>;
+  }
+}
+
+ReactDOM.render(<Cart />, document.getElementById('root'));
